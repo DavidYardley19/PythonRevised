b4bfaa23a5e0578e2216bdd1d37764e3af217edf
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2024-06-12 16:25:52 +0100

    Quick update on the exercises given by frame

--- Revision2.py
+++ Revision2.py
@@ -137,12 +137,23 @@ print()
 
 """ Python exercises provided by pictureFrame AI """
 
+"""
+Task 6
+Write a program that takes a list of integers as input and returns a new list that contains
+only the positive even numbers in the input list.
 
+Example input: [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6]
+Example output: [2, 4, 6]
+
+In the above example, the program should first filter out all the negative integers, then
+only select the even numbers, which are 2, 4, and 6.
+
+"""
 
 
 
 """ 
-
+Task 1
 Write a program that takes a list of integers as input and returns the sum of all the even
 numbers in the list.
 Example input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -157,7 +168,7 @@ for i in range(0,len(list1) + 1, 2):
 print(f"The sum of the elements in the list: {list1} is equal to {sumOfList}")
 
 """ 
-
+Task 2
 Write a program that takes a string as input and returns a new string that contains only
 the first and last characters of the input string.
 Example input: "hello"
@@ -169,7 +180,7 @@ exampleInput = input("Please enter a string, I will output the first and last ch
 print(exampleInput[0]+exampleInput[-1])
 
 """ 
-
+Task 3
 Write a program that takes two lists of integers as input and returns a new list that
 contains only the common elements between the two lists (i.e., the elements that are
 present in both lists).
@@ -190,6 +201,41 @@ print(mySet)
 newlist = list(mySet)
 print(newlist)
 # Need to complete this one
+
+""" 
+Task 4
+Write a program that takes a list of integers as input and returns a new list that contains
+only the numbers that are greater than the average of all the numbers in the input list.
+
+Example input: [10, 20, 30, 40, 50]
+Example output: [40, 50]
+
+In the above example, the average of the input list is 30, so the program should return a
+new list containing only the numbers that are greater than 30, which are 40 and 50.
+"""
+
+input1 = input("Please enter a list of integers, seperated by spaces: ")
+list1_String = input1.split(" ")
+list1_int = []
+for c in list1_String:
+    list1_int.append(int(c))
+sumOfList = sum(list1_int)
+averageOfList = sumOfList / len(list1_int)
+intAboveAverage = []
+for c in list1_int:
+    if c > averageOfList:
+        intAboveAverage.append(c)
+print(f"All values above the average = {intAboveAverage}")
+
+
+""" 
+Task 5
+Write a program that takes a string as input and returns a new string that is the reverse
+of the input string. For example, if the input string is "hello", the program should return
+"olleh".
+Example input: "hello"
+Example output: "olleh"
+"""
 
 myString = "david"
 reversedString = ""
